606276fff019ca7737e9c74cf99b88de15f47f69
aidarminyazev <legodark@mail.ru>
2023-11-25 22:05:41 +0500

    seminar3 task5 solution

--- seminar3/task5.py
+++ seminar3/task5.py
@@ -1,14 +1,28 @@
 # Переписать код в соответствии с Dependency Inversion Principle:
 
 
+from abc import ABC, abstractmethod
+
+
+class Engine(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+
+
+class PetrolEngine(Engine):
+    def start(self):
+        pass
+
+
+class DieselEngine(Engine):
+    def start(self):
+        pass
+
+
 class Car:
-    def init(self, engine):
+    def __init__(self, engine):
         self.engine = engine
 
     def start(self):
         self.engine.start()
-
-
-class PetrolEngine:
-    def start(self):
-        pass
