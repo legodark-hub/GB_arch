2f886ab0a89bb81a885e51b3026b070f171a0741
aidarminyazev <legodark@mail.ru>
2023-11-25 20:41:57 +0500

    seminar3 task1 solution

--- task1.py
+++ task1.py
@@ -1,15 +1,15 @@
-# Переписать код в соответствии с Single Responsibility Principle:
-    
-from datetime import date
 class Employee:
-def init(self, name, dob, base_salary):
-self.name = name
-self.dob = dob
-self.base_salary = base_salary
+    def __init__(self, name, dob, base_salary):
+        self.name = name
+        self.dob = dob
+        self.base_salary = base_salary
 
-def get_emp_info(self):
-    return f"name - {self.name} , dob - {self.dob}"
+    def get_emp_info(self):
+        return f"name - {self.name}, dob - {self.dob}"
 
-def calculate_net_salary(self):
-    tax = int(self.base_salary * 0.25)  # рассчитать налог другим способом
-    return self.base_salary - tax
+
+class SalaryCalculator:
+    @staticmethod
+    def calculate_net_salary(base_salary, tax_percentage):
+        tax = int(base_salary * tax_percentage)
+        return base_salary - tax
