35d696d241d92ef6f80338403f923134fa550c8e
aidarminyazev <legodark@mail.ru>
2023-11-25 21:06:58 +0500

    seminar3 task3 solution

--- seminar3/task3.py
+++ seminar3/task3.py
@@ -1,28 +1,33 @@
 # Переписать код в соответствии с Interface Segregation Principle:
+
 from abc import ABC, abstractmethod
 import math
 
-class Shape(ABC):
+
+# Интерфейс для двумерных фигур
+class TwoDimensionalShape(ABC):
     @abstractmethod
     def area(self):
         pass
 
+
+# Интерфейс для трехмерных фигур
+class ThreeDimensionalShape(ABC):
     @abstractmethod
     def volume(self):
         pass
-    
-class Circle(Shape):
-    def init(self, radius):
+
+
+class Circle(TwoDimensionalShape):
+    def __init__(self, radius):
         self.radius = radius
 
     def area(self):
         return 2 * math.pi * self.radius
 
-    def volume(self):
-        raise NotImplementedError("Circle does not have volume")
 
-class Cube(Shape):
-    def init(self, edge):
+class Cube(TwoDimensionalShape, ThreeDimensionalShape):
+    def __init__(self, edge):
         self.edge = edge
 
     def area(self):
