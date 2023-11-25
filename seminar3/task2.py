787192844f787a0b51215e2d04c477d561e9ec45
aidarminyazev <legodark@mail.ru>
2023-11-25 20:50:27 +0500

    seminar3 task2 solution

--- seminar3/task2.py
+++ seminar3/task2.py
@@ -1,24 +1,29 @@
 # Переписать код SpeedCalculation в соответствии с Open-Closed Principle:
 
-
-class SpeedCalculation:
-    def calculate_allowed_speed(self, vehicle):
-        if vehicle.get_type().lower() == "car":
-            return vehicle.get_max_speed() * 0.8
-        elif vehicle.get_type().lower() == "bus":
-            return vehicle.get_max_speed() * 0.6
-        return 0.0
-
-
 class Vehicle:
-    def init(self, max_speed, type):
+    def __init__(self, max_speed, vehicle_type):
         self.max_speed = max_speed
-        self.type = type
-
+        self.vehicle_type = vehicle_type
 
     def get_max_speed(self):
         return self.max_speed
 
-
     def get_type(self):
-        return self.type
+        return self.vehicle_type
+
+
+class Car(Vehicle):
+    def calculate_allowed_speed(self):
+        return self.get_max_speed() * 0.8
+
+
+class Bus(Vehicle):
+    def calculate_allowed_speed(self):
+        return self.get_max_speed() * 0.6
+
+
+class SpeedCalculation:
+    def calculate_allowed_speed(self, vehicle):
+        if isinstance(vehicle, Vehicle):
+            return vehicle.calculate_allowed_speed()
+        return 0.0
