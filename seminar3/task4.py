affbbc50b0f1c1e9c25fdff1748eabb6c07965a6
aidarminyazev <legodark@mail.ru>
2023-11-25 21:55:45 +0500

    seminar3 task4 solution

--- seminar3/task4.py
+++ seminar3/task4.py
@@ -1,26 +1,32 @@
 # Переписать код в соответствии с Liskov Substitution Principle:
 
 
-class Rectangle:
-    def init(self):
-        self.width = 0
-        self.height = 0
+class Shape:
+    def area(self):
+        pass
 
-    def setWidth(self, width):
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
         self.width = width
 
-    def setHeight(self, height):
+    def set_height(self, height):
         self.height = height
 
     def area(self):
         return self.width * self.height
 
 
-class Square(Rectangle):
-    def setWidth(self, width):
-        self.width = width
-        self.height = width
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
 
-    def setHeight(self, height):
-        self.width = height
-        self.height = height
+    def set_side(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
